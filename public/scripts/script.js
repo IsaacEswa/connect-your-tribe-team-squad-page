fcf71462a9d9c68ffe497fe84d8347f860737e0d
IsaacEswa <isaaceswa@gmail.com>
2026-02-17 15:20:06 +0100

    added working like counter with JS #7

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -111,20 +111,73 @@ views.forEach(link => {
 
 
 
+// const likeContainer = document.querySelector(".like-container");
+// const likeButton = document.querySelector(".like-button");
+
+// likeButton.addEventListener('click', addLiked);
+
+// function addLiked() {
+//     likeContainer.classList.toggle('liked');
+//     likeContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M241 87.1l15 20.7 15-20.7C296 52.5 336.2 32 378.9 32 452.4 32 512 91.6 512 165.1l0 2.6c0 112.2-139.9 242.5-212.9 298.2-12.4 9.4-27.6 14.1-43.1 14.1s-30.8-4.6-43.1-14.1C139.9 410.2 0 279.9 0 167.7l0-2.6C0 91.6 59.6 32 133.1 32 175.8 32 216 52.5 241 87.1z"/></svg>
+//                 <p class="like-counter">0</p>
+// `
+// }
+
+// document.querySelectorAll('.like-container').forEach(container => {
+//     const likeButton = container.querySelector('.like-button');
+//     const likeCounter = container.querySelector('.like-counter');
+//     let count = 0;
+
+//     likeButton.addEventListener('click', () => {
+//         container.classList.toggle('liked');
+//         count += container.classList.contains('liked') ? 1 : -1;
+//         likeCounter.textContent = count;
+//     });
+// });
+
+
+const regularHeart = `M378.9 80c-27.3 0-53 13.1-69 35.2l-34.4 47.6c-4.5 6.2-11.7 9.9-19.4 9.9s-14.9-3.7-19.4-9.9l-34.4-47.6c-16-22.1-41.7-35.2-69-35.2-47 0-85.1 38.1-85.1 85.1 0 49.9 32 98.4 68.1 142.3 41.1 50 91.4 94 125.9 120.3 3.2 2.4 7.9 4.2 14 4.2s10.8-1.8 14-4.2c34.5-26.3 84.8-70.4 125.9-120.3 36.2-43.9 68.1-92.4 68.1-142.3 0-47-38.1-85.1-85.1-85.1zM271 87.1c25-34.6 65.2-55.1 107.9-55.1 73.5 0 133.1 59.6 133.1 133.1 0 68.6-42.9 128.9-79.1 172.8-44.1 53.6-97.3 100.1-133.8 127.9-12.3 9.4-27.5 14.1-43.1 14.1s-30.8-4.7-43.1-14.1C176.4 438 123.2 391.5 79.1 338 42.9 294.1 0 233.7 0 165.1 0 91.6 59.6 32 133.1 32 175.8 32 216 52.5 241 87.1l15 20.7 15-20.7z`;
+const solidHeart = `M241 87.1l15 20.7 15-20.7C296 52.5 336.2 32 378.9 32 452.4 32 512 91.6 512 165.1l0 2.6c0 112.2-139.9 242.5-212.9 298.2-12.4 9.4-27.6 14.1-43.1 14.1s-30.8-4.6-43.1-14.1C139.9 410.2 0 279.9 0 167.7l0-2.6C0 91.6 59.6 32 133.1 32 175.8 32 216 52.5 241 87.1z`;
+
+document.querySelectorAll('.like-container').forEach(container => {
+    const likeButton = container.querySelector('.like-button');
+    const likeCounter = container.querySelector('.like-counter');
+    const heartPath = container.querySelector('.heart-path');
+
+    let count = 0;
+
+    likeButton.addEventListener('click', () => {
+        container.classList.toggle('liked');
+
+        const liked = container.classList.contains('liked');
+
+        // counter
+        count += liked ? 1 : -1;
+        likeCounter.textContent = count;
+
+        // heart switch (regular ↔ solid)
+        heartPath.setAttribute('d', liked ? solidHeart : regularHeart);
+    });
+});
+
+
+console.log(likeButton, likeContainer);
+
 
 //######################## Seb ################################
 
 let dark = document.querySelector('.1j');
 let light = document.querySelector('.1i')
 
-dark.addEventListener('click', function(){
+dark.addEventListener('click', function () {
     dark.classList.add('dark-color')
     light.classList.add('opacity-text')
     dark.classList.remove('opacity-text')
 })
 
-light.addEventListener('click', function(){
+light.addEventListener('click', function () {
     dark.classList.remove('dark-color')
     dark.classList.add('opacity-text')
     light.classList.remove('opacity-text')
 })
+
